323e3705817836caf4482173b48f03ee369c1776
Omar Morad <omorad@students.cs.ubc.ca>
2025-02-06 17:18:21 -0800

    added specification for card

---
+++ src/main/model/Card.java
@@ -0,0 +1,39 @@
+package model;
+
+public class Card {
+
+    // MODIFIES : this
+    // EFFECTS : constructs a card with a rank and suit
+    public Card() {
+
+    }
+
+
+    // EFFECTS : returns the chips this card provides
+    public int getChips() {
+        return 0;
+    }
+
+
+    // EFFECTS : returns the mult this card provides
+    public int getMult() {
+        return 0;
+    }
+
+
+    public String getRank() {
+        return "";
+    }
+
+    public String getSuit() {
+        return "";
+    }
+
+    // EFFECTS : returns the name of the card (rank and suit)
+    public String getCard() {
+        return "";
+    }
+
+
+
+}
