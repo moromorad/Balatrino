25317ab895c9ba2e2722c0348ae28e2ba4f37fc8
Omar Morad <omorad@students.cs.ubc.ca>
2025-02-06 22:37:00 -0800

    changed the Joker interface

--- src/main/model/Joker.java
+++ src/main/model/Joker.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 // represents a Joker, every joker has an ability and a descrition describing that ability.
 public interface Joker {
-    public List<Integer> Ability(int chips, int mult, List<Card> playedHand);
+    public void Ability(List<Integer> chipsAndMult, List<Card> playedHand);
     public String getDesc();
 
 }
