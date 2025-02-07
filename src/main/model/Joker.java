b77fa124b7e59d74d6324ef2af97d06522720852
Omar Morad <omorad@students.cs.ubc.ca>
2025-02-06 21:30:32 -0800

    implemented an interface for jokers

---
+++ src/main/model/Joker.java
@@ -0,0 +1,8 @@
+package model;
+
+// represents a Joker, every joker has an ability and a descrition describing that ability.
+public interface Joker {
+    public int Ability(int chips, int mult);
+    public String getDesc();
+
+}
