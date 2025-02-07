778f854464bfe07fb0dd390d7fe00cdabf354375
Omar Morad <omorad@students.cs.ubc.ca>
2025-02-06 21:46:09 -0800

    edited Joker interface

--- src/main/model/Joker.java
+++ src/main/model/Joker.java
@@ -1,8 +1,10 @@
 package model;
 
+import java.util.List;
+
 // represents a Joker, every joker has an ability and a descrition describing that ability.
 public interface Joker {
-    public int Ability(int chips, int mult);
+    public List<Integer> Ability(int chips, int mult, List<Card> playedHand);
     public String getDesc();
 
 }
