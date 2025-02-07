a38b0a1e0fcb585eeac91bf0f458ee0616b7aa19
Omar Morad <omorad@students.cs.ubc.ca>
2025-02-06 23:08:39 -0800

    implemented OddJoker

--- src/main/model/OddJoker.java
+++ src/main/model/OddJoker.java
@@ -8,11 +8,19 @@ public class OddJoker implements Joker {
         desc = "Played cards with even rank each give +4 Mult";
     }
 
-    // EFFECT: each even ranked card in the hand adds +4 Mult
+    // EFFECT: each even ranked card in the hand adds +4 Mult (2,4,6,8,10)
     @Override
     public void Ability(List<Integer> chipsAndMult, List<Card> playedHand) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'Ability'");
+        int mult = 0;
+        for (Card card : playedHand) {
+            if (card.getRank().matches("\\d+")) {
+                if (Integer.parseInt(card.getRank()) % 2 == 0) {
+                    mult = mult + 4;
+                }
+            }
+        }
+        chipsAndMult.set(1, chipsAndMult.get(1) + mult);
+       
     }
 
     @Override
