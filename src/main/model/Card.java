d1e8d4000d20fec0039bb4dcde359b9a93dd4d31
Omar Morad <omorad@students.cs.ubc.ca>
2025-03-22 02:23:27 -0700

    overrided hashcode and equals

--- src/main/model/Card.java
+++ src/main/model/Card.java
@@ -20,39 +20,6 @@ public class Card {
     }
 
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((rank == null) ? 0 : rank.hashCode());
-        result = prime * result + ((suit == null) ? 0 : suit.hashCode());
-        return result;
-    }
-
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Card other = (Card) obj;
-        if (rank == null) {
-            if (other.rank != null)
-                return false;
-        } else if (!rank.equals(other.rank))
-            return false;
-        if (suit == null) {
-            if (other.suit != null)
-                return false;
-        } else if (!suit.equals(other.suit))
-            return false;
-        return true;
-    }
-
-
     // EFFECTS : returns the chips this card provides
     public int getChips() {
         if (getRank().matches("\\d+")) {
@@ -118,6 +85,38 @@ public class Card {
         JSONObject json = new JSONObject();
         json.put("name", getCard());
         return json;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((rank == null) ? 0 : rank.hashCode());
+        result = prime * result + ((suit == null) ? 0 : suit.hashCode());
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Card other = (Card) obj;
+        if (rank == null) {
+            if (other.rank != null)
+                return false;
+        } else if (!rank.equals(other.rank))
+            return false;
+        if (suit == null) {
+            if (other.suit != null)
+                return false;
+        } else if (!suit.equals(other.suit))
+            return false;
+        return true;
     }
 
 
