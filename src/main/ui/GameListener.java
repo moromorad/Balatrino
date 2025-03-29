25bbf11a156edaa7892203b209eb456f5ee8697e
Omar Morad <omorad@students.cs.ubc.ca>
2025-03-29 00:39:16 -0700

    documentaion

--- src/main/ui/GameListener.java
+++ src/main/ui/GameListener.java
@@ -9,6 +9,7 @@ import model.EventLog;
 // Prints the event log when a frame that has this added as its WindowListener
 public class GameListener extends WindowAdapter {
     @Override
+    // EFFECTS : prints the event log when the frame is closed then ends the application
     public void windowClosing(WindowEvent e) {
         EventLog eventLog = EventLog.getInstance();
         for (Event event : eventLog) {
